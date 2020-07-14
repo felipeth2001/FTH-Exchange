5123f85fdcfd2711698428f7bad765ab4131aeef
Juan Felipe Tamayo Herrera <juanth2001@hotmail.com>
2020-07-13 22:09:36 -0500

    integración de pantalla de carga y grafico

--- src/router.js
+++ src/router.js
@@ -1,8 +1,11 @@
 import Vue from "vue";
 import Router from "vue-router";
+
+
 import Home from "@/Views/Home";
 import About from "@/Views/About";
 import Error from "@/Views/Error";
+import CoinDetail from "@/Views/CoinDetail";
 
 Vue.use(Router);
 
@@ -20,6 +23,12 @@ export default new Router({
       path: "/about",
       name: "about",
       component: About
+    },
+
+    {
+      path:'/coin/:id',
+      name:'coin-detail',
+      component: CoinDetail
     },
 
     {
